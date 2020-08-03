5b3ea2772f4d53b36748f1ca6eb902ddc6086e1a
dooking <123456ghghgh@naver.com>
2020-08-04 01:52:15 +0900

    django 초기설정

---
+++ project/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+#from .models import Post, Product, Comment,UserInfo,Like,Survey,CardNews_model, Community_model,Community_comment
+
+# Register your models here.
+#admin.site.register(Post)
