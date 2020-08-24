f393f3e6fc0742c5a721fd0a5e49904cd27ccef2
2oneweek <dlgkswn885@korea.ac.kr>
2020-08-24 21:40:17 +0900

    관리자 페이지 뼈대 html 및 장비 model 교체

--- project/adminpage/urls.py
+++ project/adminpage/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from adminpage import views
 
+
+# TODO:url 손보기  including 되는거라 ㅇㅇ
 urlpatterns = [
     path('main', views.main, name='main'),
+    path('total', views.total, name='total'),
+    path('equipment', views.equipment, name='equipment'),
+    path('addequipment', views.addEquipment, name='addEquipment'),
+    path('qrcheck', views.qrcheck, name='qrcheck')
 ]
