93325b868889e401702272ce9a9f0ba4f5b00b5f
2oneweek <dlgkswn885@korea.ac.kr>
2020-09-12 23:55:09 +0900

    total page 자스 수정

--- project/adminpage/static/total.js
+++ project/adminpage/static/total.js
@@ -1,27 +1,28 @@
 function init() {
-  const htmllistDevice = document.getElementById("htmllist");
-  const equipmentList = document.getElementById("equipmentList");
+  const equipmentLists = document.querySelectorAll(".equipmentList");
+  console.log(equipmentLists);
+  for (let equipmentList of equipmentLists) {
+    if (equipmentList.value.includes("@@")) {
+      const newlistsDevice = equipmentList.value.split("@@");
+      for (let newlistDevice of newlistsDevice) {
+        let item = document.createElement("p");
+        const itemAttr = newlistDevice.split("^").join(" ");
+        item.innerText = itemAttr;
+        equipmentList.parentNode.appendChild(item);
+      }
+    } else {
+      const newlistsDevice = equipmentList.value
+        .slice(1, -1)
+        .replace(/'/g, "")
+        .replace(/ /g, "")
+        .split(",");
+      for (let newlistDevice of newlistsDevice) {
+        let item = document.createElement("p");
 
-  if (equipmentList.value.includes("@@")) {
-    const newlistsDevice = equipmentList.value.split("@@");
-    for (let newlistDevice of newlistsDevice) {
-      let item = document.createElement("p");
-      const itemAttr = newlistDevice.split("^").join(" ");
-      item.innerText = itemAttr;
-      htmllistDevice.appendChild(item);
-    }
-  } else {
-    const newlistsDevice = equipmentList.value
-      .slice(1, -1)
-      .replace(/'/g, "")
-      .replace(/ /g, "")
-      .split(",");
-    for (let newlistDevice of newlistsDevice) {
-      let item = document.createElement("p");
-
-      const itemAttr = newlistDevice.split(":");
-      item.innerText = itemAttr[0] + "    " + itemAttr[1] + "개";
-      htmllistDevice.appendChild(item);
+        const itemAttr = newlistDevice.split(":");
+        item.innerText = itemAttr[0] + "    " + itemAttr[1] + "개";
+        equipmentList.parentNode.appendChild(item);
+      }
     }
   }
 }
