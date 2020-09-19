4b3161be0ffff05731b8eeb6dc53983af6e81547
2oneweek <dlgkswn885@korea.ac.kr>
2020-09-20 00:26:18 +0900

    auth 경로 처리

--- project/app/static/3-borrow/step1.js
+++ project/app/static/3-borrow/step1.js
@@ -1,158 +1,152 @@
-let tempList = new Map()
+let tempList = new Map();
 
 function hide(event) {
-    const hide = event.nextElementSibling;
-    if(hide.classList.contains("mystyle")){
-        hide.classList.remove("mystyle")
-        event.childNodes[3].innerText = "▲"
-    }
-    else{
-        hide.classList.add("mystyle")
-        event.childNodes[3].innerText = "▼"
-    }
+  const hide = event.nextElementSibling;
+  if (hide.classList.contains("mystyle")) {
+    hide.classList.remove("mystyle");
+    event.childNodes[3].innerText = "▲";
+  } else {
+    hide.classList.add("mystyle");
+    event.childNodes[3].innerText = "▼";
+  }
 }
 
+function radioHandler(cb) {
+  const borrowList = document.querySelectorAll("li");
+  const borrowClass = document.querySelector(".borrow_list");
+  const addItem = document.createElement("li");
+  let check = 0;
+  let index = 0;
+  for (let i = 0; i < borrowList.length; i++) {
+    if (borrowList[i].className === "camera") {
+      console.log("Cb.value : ", cb.value);
+      console.log("여기 : ", borrowList[i].textContent);
 
-function radioHandler(cb){
-    const borrowList = document.querySelectorAll("li")
-    const borrowClass = document.querySelector(".borrow_list")
-    const addItem = document.createElement('li')
-    let check = 0
-    let index = 0
-
-    for(let i=0; i<borrowList.length; i++){
-        if(borrowList[i].className === "camera"){
-            console.log("Cb.value : ", cb.value)
-            console.log("여기 : ", borrowList[i].textContent)
-            if(cb.value === borrowList[i].textContent.slice(0,borrowList[i].textContent.length-3)){
-                check = 2
-            }
-            else{
-                check = 1
-            }
-            index = i
-            break;
-        }
+      if (
+        cb.value ===
+        borrowList[i].textContent.slice(0, borrowList[i].textContent.length - 3)
+      ) {
+        check = 2;
+      } else {
+        check = 1;
+      }
+      index = i;
+      break;
     }
-    // 다른 종류 선택할 때
-    if(check === 1){
-        borrowList[index].textContent = cb.value +" "+"1개"
-        tempList.set(cb.id,cb.value)
-    }
-    // 같은 종류 선택할 때
-    else if(check === 2){
-        borrowClass.removeChild(borrowList[index])
-        cb.checked = false
-        tempList.delete(cb.id)
-    }
-    else{
-        addItem.setAttribute("class","camera")
-        addItem.textContent = cb.value +" "+"1개"
-        borrowClass.appendChild(addItem)
-        tempList.set(cb.id,cb.value)
-    }
+  }
+  // 다른 종류 선택할 때
+  if (check === 1) {
+    borrowList[index].textContent = cb.value + " " + "1개";
+    tempList.set(cb.id, cb.value);
+  }
+  // 같은 종류 선택할 때
+  else if (check === 2) {
+    borrowClass.removeChild(borrowList[index]);
+    cb.checked = false;
+    tempList.delete(cb.id);
+  } else {
+    addItem.setAttribute("class", "camera");
+    addItem.textContent = cb.value + " " + "1개";
+    borrowClass.appendChild(addItem);
+    tempList.set(cb.id, cb.value);
+  }
 }
-function checkHandler(cb){
-    const borrowList = document.querySelectorAll("li")
-    const borrowClass = document.querySelector(".borrow_list")
-    const addItem = document.createElement('li')
-    let check = false
-    let index = 0
-    for(let i=0; i<borrowList.length; i++){
-        if(borrowList[i].className === `${cb.id}`){
-            check = true
-            index = i
-            break;
-        }
+function checkHandler(cb) {
+  const borrowList = document.querySelectorAll("li");
+  const borrowClass = document.querySelector(".borrow_list");
+  const addItem = document.createElement("li");
+  let check = false;
+  let index = 0;
+  for (let i = 0; i < borrowList.length; i++) {
+    if (borrowList[i].className === `${cb.id}`) {
+      check = true;
+      index = i;
+      break;
     }
-    if(check){
-        borrowClass.removeChild(borrowList[index])
-        tempList.delete(cb.value)
-    }
-    else{
-        addItem.setAttribute("class",`${cb.id}`)
-        addItem.textContent = cb.value +" "+"1개"
-        borrowClass.appendChild(addItem)
-        tempList.set(cb.value,"1")
-    }
-}
-
-function numberHandler(cb){
-    const borrowClass = document.querySelector(".borrow_list")
-    const borrowList = document.querySelectorAll("li")
-    const addItem = document.createElement('li')
-    const countSelect = document.querySelector(".countSelect")
-    const etc = document.querySelector("#etc")
-    let check = false
-    let index = 0
-
-    for(let i=0; i<borrowList.length; i++){
-        if(borrowList[i].className === `${cb.id}`){
-            check = true
-            index = i
-            break;
-        }
-    }
-    // 해당 값이 0일 때
-    if(check && cb.value == 0){
-        borrowClass.removeChild(borrowList[index])
-        tempList.delete(cb.id)
-        countSelect.classList.toggle("mystyle")
-        etc.checked = false
-    }
-    else{
-        // 해당 값이 이미 있을 때
-        if(check){
-            borrowList[index].textContent = cb.id +" "+cb.value +"개"
-            tempList.set(cb.id,cb.value)
-        }
-        else{
-            addItem.setAttribute("class",`${cb.id}`)
-            addItem.textContent = cb.id +" "+cb.value +"개"
-            borrowClass.appendChild(addItem)
-            tempList.set(cb.id,cb.value)
-        }
-    }
+  }
+  if (check) {
+    borrowClass.removeChild(borrowList[index]);
+    tempList.delete(cb.value);
+  } else {
+    addItem.setAttribute("class", `${cb.id}`);
+    addItem.textContent = cb.value + " " + "1개";
+    borrowClass.appendChild(addItem);
+    tempList.set(cb.value, "1");
+  }
 }
 
-function etcHandler(cb){
-    const countSelect = document.querySelector(".countSelect")  
-    const borrowClass = document.querySelector(".borrow_list")
-    const borrowList = document.querySelectorAll("li")
-    const addItem = document.createElement('li')  
-    let index = 0
+function numberHandler(cb) {
+  const borrowClass = document.querySelector(".borrow_list");
+  const borrowList = document.querySelectorAll("li");
+  const addItem = document.createElement("li");
+  const countSelect = document.querySelector(".countSelect");
+  const etc = document.querySelector("#etc");
+  let check = false;
+  let index = 0;
 
-    for(let i=0; i<borrowList.length; i++){
-        if(borrowList[i].className === `${countSelect.id}`){
-            index = i
-            break;
-        }
+  for (let i = 0; i < borrowList.length; i++) {
+    if (borrowList[i].className === `${cb.id}`) {
+      check = true;
+      index = i;
+      break;
     }
-    if(countSelect.classList.contains("mystyle")){
-        countSelect.classList.remove("mystyle")
-        if(countSelect.value != 0){
-            addItem.setAttribute("class",`${countSelect.id}`)
-            addItem.textContent = countSelect.id +" "+countSelect.value +"개"
-            borrowClass.appendChild(addItem)
-            tempList.set(countSelect.id,countSelect.value)
-        }
+  }
+  // 해당 값이 0일 때
+  if (check && cb.value == 0) {
+    borrowClass.removeChild(borrowList[index]);
+    tempList.delete(cb.id);
+    countSelect.classList.toggle("mystyle");
+    etc.checked = false;
+  } else {
+    // 해당 값이 이미 있을 때
+    if (check) {
+      borrowList[index].textContent = cb.id + " " + cb.value + "개";
+      tempList.set(cb.id, cb.value);
+    } else {
+      addItem.setAttribute("class", `${cb.id}`);
+      addItem.textContent = cb.id + " " + cb.value + "개";
+      borrowClass.appendChild(addItem);
+      tempList.set(cb.id, cb.value);
     }
-    else{
-        countSelect.classList.add("mystyle")
-        borrowClass.removeChild(borrowList[index])
-        tempList.delete(countSelect.id)
+  }
+}
+
+function etcHandler(cb) {
+  const countSelect = document.querySelector(".countSelect");
+  const borrowClass = document.querySelector(".borrow_list");
+  const borrowList = document.querySelectorAll("li");
+  const addItem = document.createElement("li");
+  let index = 0;
+
+  for (let i = 0; i < borrowList.length; i++) {
+    if (borrowList[i].className === `${countSelect.id}`) {
+      index = i;
+      break;
     }
+  }
+  if (countSelect.classList.contains("mystyle")) {
+    countSelect.classList.remove("mystyle");
+    if (countSelect.value != 0) {
+      addItem.setAttribute("class", `${countSelect.id}`);
+      addItem.textContent = countSelect.id + " " + countSelect.value + "개";
+      borrowClass.appendChild(addItem);
+      tempList.set(countSelect.id, countSelect.value);
+    }
+  } else {
+    countSelect.classList.add("mystyle");
+    borrowClass.removeChild(borrowList[index]);
+    tempList.delete(countSelect.id);
+  }
 }
 
 //time block
-for (let index=0; index<18; index++){
-    let blockCount = document.getElementsByClassName(String(index))
-    for (let i=0; i<blockCount.length; i++)
-    {
-        for (let j=0; j<blockCount[i].id; j++){
-            const newDiv = document.createElement('div')
-            newDiv.setAttribute("class","time-block")
-            blockCount[i].appendChild(newDiv);
-        }
+for (let index = 0; index < 18; index++) {
+  let blockCount = document.getElementsByClassName(String(index));
+  for (let i = 0; i < blockCount.length; i++) {
+    for (let j = 0; j < blockCount[i].id; j++) {
+      const newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "time-block");
+      blockCount[i].appendChild(newDiv);
     }
+  }
 }
